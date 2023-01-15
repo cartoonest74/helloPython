721711d4c2c251c11e224fa8650114d73883dce4
cartoonest74 <gyeng74@gmail.com>
2023-01-15 19:39:23 +0900

    edit sys1.py

--- mymodules/sys1.py
+++ mymodules/sys1.py
@@ -1,12 +1,25 @@
-import sys
+import sys, os
 
 print(sys.argv, len(sys.argv))
 
+dir_name = os.path.dirname(__file__)
+abs_path = os.path.abspath(dir_name)
+hello_path = os.path.join(abs_path, '..')
+sys.path.append(hello_path) 
+print('dir_name >> ', dir_name)
+print('abs_path >> ', abs_path)
+print('sys_path >>',hello_path)
+
+# clear module
+import mysys1
+mysys1.clear()
+
+# argv 변수 
 sa = sys.argv
 if len(sa) < 2:
     sys.exit()
 
-with open(argv[1], "r", encoding="utf-8") as file:
+with open(sa[1], "r", encoding="utf-8") as file:
     for line in file:
         print(line)
  
