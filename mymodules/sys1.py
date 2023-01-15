0546e1ff510d6ddd1968fb0828b9e57c855a008b
cartoonest74 <gyeng74@gmail.com>
2023-01-15 18:42:51 +0900

    patch test

--- mymodules/sys1.py
+++ mymodules/sys1.py
@@ -1,9 +1,25 @@
-import sys
-
-print(sys.argv, len(sys.argv))
+import sys,os
 
 sa = sys.argv
+print(sa, len(sa))
+
+print('__file__ ==>',__file__)
+print('dirname ==>',os.path.dirname(__file__))
+
+dir_path = os.path.dirname(__file__)
+abs_path = os.path.abspath(dir_path)
+basea_path = os.path.join(abs_path,'..')
+sys.path.append(basea_path)
+print('abspath ==>',abs_path)
+print('sys_path ==>',sys.path)
+
+import mysys1
+
+mysys1.clear()
+
+
 if len(sa) < 2:
+    print('')
     sys.exit()
 
 with open(sa[1], 'r', encoding="utf-8") as file:
